57c7480d16608c06b5aa82040557e7ca5be18239
sahanasadagopan <sahana.sadagopan@gmail.com>
2017-06-11 03:20:22 -1200

    added comments

--- Sources/capacitive_touch.c
+++ Sources/capacitive_touch.c
@@ -14,8 +14,8 @@ static int i = 0;
 
 void TSI_init()
 {
-	SIM_SCGC5 |= SIM_SCGC5_TSI_MASK;
-	SIM_SCGC5 |= SIM_SCGC5_PORTB_MASK;
+	SIM_SCGC5 |= SIM_SCGC5_TSI_MASK; // Enabling TSI access control
+	SIM_SCGC5 |= SIM_SCGC5_PORTB_MASK; // clock for portB
 
 	TSI0_GENCS|=TSI_GENCS_MODE(0);
 	TSI0_GENCS|=TSI_GENCS_REFCHRG(2);//2uA discharge
@@ -25,20 +25,19 @@ void TSI_init()
 	TSI0_GENCS|=TSI_GENCS_NSCN(15);//16 SCANS
 	TSI0_GENCS|=TSI_GENCS_STPE_MASK;
 	TSI0_GENCS|= TSI_GENCS_TSIEN_MASK;
-	//PORTB_PCR18=PORT_PCR_MUX(0);//SELECT CHANNEL 11
-	//PORTB_PCR19=PORT_PCR_MUX(0);//SELECT CHANNEL 12
+	
 	PORTB_PCR16 = PORT_PCR_MUX(0);      // PTB16 as touch channel 9
 	PORTB_PCR17 = PORT_PCR_MUX(0);      // PTB17 as touch channel 10
 }
 
 void TSI_Start(int CH_IP)
 {
-	TSI0_DATA|=TSI_DATA_TSICH(CH_IP);
-	TSI0_DATA|=TSI_DATA_SWTS_MASK;
+	TSI0_DATA|=TSI_DATA_TSICH(CH_IP);// selecting TSI channel through the Data register
+	TSI0_DATA|=TSI_DATA_SWTS_MASK;// Start scanning 
 }
 
 uint16_t TSI_Stop_check()
 {
-	TSI0_GENCS |= TSI_GENCS_EOSF_MASK;
-	return TSI0_DATA & TSI_DATA_TSICNT_MASK;
+	TSI0_GENCS |= TSI_GENCS_EOSF_MASK; // checking for software aknowledgement 
+	return TSI0_DATA & TSI_DATA_TSICNT_MASK; // return the Data value and the scan counter value got by the oscilator
 }
